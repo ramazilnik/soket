61e9817a0594d19bb43f64c7d4b2cf58eaf6470e
Ramazan <faraonramzess@gmail.com>
2023-09-01 17:12:54 +0600

    serv

---
+++ server_sock.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+host = "127.0.0.1"
+port = 50001
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(host, port)
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def recive()
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+        
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        #threading
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening...")
+recive()
+
